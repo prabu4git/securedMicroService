63637de5ad49b23b488649c7b3575284286c394c
Prabu Murugesan <prarun.pm@gmail.com>
2019-01-02 18:32:31 -0500

    Initial Commit

---
+++ src/main/java/com/ford/powertrain/etb/ETargetBookApplication.java
@@ -0,0 +1,17 @@
+package com.ford.powertrain.etb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableWebSecurity
+public class ETargetBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ETargetBookApplication.class, args);
+	}
+}
+
---
+++ src/main/java/com/ford/powertrain/etb/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.ford.powertrain.etb.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/secured/welcome")
+public class HelloController {
+	
+	@GetMapping
+	public String welcome() {
+		return "Welcome to the world of spring security";
+		
+	}
+	
+}
